d82630225e0729e17d61770329dbb5636fb117bf
mavidus <mavdus2012@gmail.com>
2019-09-23 12:36:37 -0600

    commit 4. add negative

--- src/test/java/test/LoginTests.java
+++ src/test/java/test/LoginTests.java
@@ -4,9 +4,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -22,26 +25,31 @@ public class LoginTests {
         driver =new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-
     }
 
-    @Test
+    @Test(priority =0)
     public void loginTest1(){
         driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx?ReturnUrl=%2fsamples%2ftestcomplete12%2fweborders%2fDefault.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
-
         Assert.assertEquals(driver.getTitle(), "Web Orders");
-
+        driver.findElement(By.id("ctl00_logout")).click();
 
     }
 
+    @Test(priority =1)
+    public void NegativeoginTest1(){
+        driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx?ReturnUrl=%2fsamples%2ftestcomplete12%2fweborders%2fDefault.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("invalid" + Keys.ENTER);
+        String errorMsg=driver.findElement(By.id("ctl00_MainContent_status")).getText();
+        Assert.assertEquals(errorMsg, "Invalid Login or Password.");
+    }
 
-    @AfterMethod
+    @AfterTest
     public void LogoutTest() throws InterruptedException {
-        driver.findElement(By.id("ctl00_logout")).click();
-        Assert.assertEquals(driver.getTitle(), "Web Orders Login");
-        Thread.sleep(5000);
+       // driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx?ReturnUrl=%2fsamples%2ftestcomplete12%2fweborders%2fDefault.aspx");
+        Thread.sleep(3000);
              driver.close();
         }
 }
