fd2651cff7198cb7834e80230c267bf010d492f1
mavidus <mavdus2012@gmail.com>
2019-09-23 10:16:57 -0600

    first commit

---
+++ src/test/java/test/LoginTests.java
@@ -0,0 +1,37 @@
+package test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTests {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver =new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+    }
+
+    @Test
+    public void loginTest1(){
+        driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx?ReturnUrl=%2fsamples%2ftestcomplete12%2fweborders%2fDefault.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
+
+        Assert.assertEquals(driver.getTitle(), "Web Orders");
+
+/Users/mavidus/Documents/Java/src/test/java
+    }
+}
